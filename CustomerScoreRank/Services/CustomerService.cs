b0485b046e34596ab192066ab5eee8b500c8dcdf
Solution_X <xiongyu132@163.com>
2022-07-29 13:05:39 +0800

    代码优化

--- CustomerScoreRank/Services/CustomerService.cs
+++ CustomerScoreRank/Services/CustomerService.cs
@@ -1,16 +1,22 @@
 ﻿using CustomerScoreRank.Models;
+using System.Collections.Concurrent;
 
 namespace CustomerScoreRank.Services
 {
     public class CustomerService : ICustomerService
     {
-        private readonly List<Customer> _customers;
+        private static readonly List<Customer> _customers = new();
 
         public CustomerService()
         {
-            _customers = new List<Customer>();
         }
 
+        /// <summary>
+        /// 更新顾客的分数（累加）
+        /// </summary>
+        /// <param name="customerId">顾客Id</param>
+        /// <param name="score">分数</param>
+        /// <returns></returns>
         public async Task<decimal> UpdateScore(long customerId, decimal score)
         {
             decimal finalScore;
@@ -32,18 +38,20 @@ namespace CustomerScoreRank.Services
             return await Task.FromResult(finalScore);
         }
 
+        /// <summary>
+        /// 根据排名获得顾客列表
+        /// </summary>
+        /// <param name="start">起始排名</param>
+        /// <param name="end">结束排名</param>
+        /// <returns></returns>
         public async Task<List<CustomerScoreRankInfo>> GetCustomersByRank(int start, int end)
         {
-            var matchedCustomers = _customers
-                .OrderByDescending(x => x.Score).ThenBy(x => x.Id)
-                .Skip(start - 1)
-                .Take(end - start + 1)
-                .ToList();
+            List<Customer> matchedCustomers = GetCustomersFromOrderedList(start - 1, end - start + 1);
 
-            var result = new List<CustomerScoreRankInfo>();
+            var resultBag = new ConcurrentBag<CustomerScoreRankInfo>();
             Parallel.For(0, matchedCustomers.Count, i =>
             {
-                result.Add(new CustomerScoreRankInfo
+                resultBag.Add(new CustomerScoreRankInfo
                 {
                     CustomerId = matchedCustomers[i].Id,
                     Score = matchedCustomers[i].Score,
@@ -51,22 +59,48 @@ namespace CustomerScoreRank.Services
                 });
             });
 
-            return await Task.FromResult(result.OrderBy(x => x.Rank).ToList());
+            return await Task.FromResult(resultBag.OrderBy(x => x.Rank).ToList());
         }
 
+        /// <summary>
+        /// 从以排序的顾客列表中获取顾客列表
+        /// </summary>
+        /// <param name="offset">偏移量</param>
+        /// <param name="tackCount">获取数量</param>
+        /// <returns></returns>
+        private List<Customer> GetCustomersFromOrderedList(int offset, int tackCount)
+        {
+            return _customers
+                .OrderByDescending(x => x.Score).ThenBy(x => x.Id)
+                .Skip(offset)
+                .Take(tackCount)
+                .ToList();
+        }
+
+        /// <summary>
+        /// 根据顾客Id获取顾客所处排名前[high]、后[low]个顾客的数据
+        /// </summary>
+        /// <param name="customerId">顾客Id</param>
+        /// <param name="high">排在目标顾客前面的顾客个数</param>
+        /// <param name="low">排在目标顾客后面的顾客个数</param>
+        /// <returns></returns>
         public async Task<List<CustomerScoreRankInfo>> GetCustomersByCustomerId(long customerId, int high = 0, int low = 0)
         {
-            var indexOfTargetCustomer = _customers
-                .OrderByDescending(x => x.Score).ThenBy(x => x.Id)
-                .ToList()
-                .FindIndex(x => x.Id == customerId);
+            // 获取目标顾客在分数排名中的位置（列表索引）
+            var indexOfTargetCustomer = GetCustomersFromOrderedList(0, _customers.Count).FindIndex(x => x.Id == customerId);
 
+            // 防止索引溢出
             high = indexOfTargetCustomer - high < 0 ? 0 : high;
             low = indexOfTargetCustomer + low > _customers.Count - 1 ? _customers.Count - 1 : low;
 
             return await GetCustomersByRank(indexOfTargetCustomer - high + 1, indexOfTargetCustomer + low + 1);
         }
 
+        /// <summary>
+        /// 根据顾客Id判断顾客是否存在
+        /// </summary>
+        /// <param name="customerId">顾客Id</param>
+        /// <returns></returns>
         public async Task<bool> IsCustomerExist(long customerId)
         {
             return await Task.FromResult(_customers.Any(x => x.Id == customerId));
